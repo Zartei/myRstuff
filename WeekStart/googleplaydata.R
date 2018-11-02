23ec4213dde056849cd82394a64ecd7c9281fa77
Zartei <Zirdii@gmail.com>
2018-11-02 08:45:33 +0100

    PartOneDone

--- WeekStart/googleplaydata.R
+++ WeekStart/googleplaydata.R
@@ -27,11 +27,13 @@ gplay <- read.csv("googleplaystore.csv")
 # Andriod.Ver
 gplay <- gplay[-c(1, 2, 11, 12, 13)]
 
+gplay$Reviews <- as.numeric(gplay$Reviews)
 # sum(apply(gplay, 2, is.na)) # 1474
 # nrow(gplay) # 10841
 # nrow(na.omit(gplay)) # Check to see if it is okey todrop rows with na ~10% of data set so no good.
 # table(gplay$Rating, useNA = "ifany")
 # Find that it is the raiting that has alot of missing values.
+
 gplay$Rating <- as.factor(gplay$Rating)
 levels(gplay$Rating)[levels(gplay$Rating) == 'NaN'] <- "Missing"
 # sum(is.na(gplay$Rating))
@@ -51,9 +53,9 @@ levels(gplay$Type)[3] = "missing";
 gplay_set <- splitTrainSet(gplay, split = 0.9)
 
 
-c5_model <- C5.0(gplay_set$train[c(1,2,3,4,5,6,7)], gplay_set$train$Genres)
+c5_model <- C5.0(gplay_set$train[c(1,2,3,5,6,7)], gplay_set$train$Genres, trails =3)
 
-c5_prediction <- predict(c5_model, gplay_set$test[c(1, 2, 3, 4, 5, 6, 7)])
+c5_prediction <- predict(c5_model, gplay_set$test[c(1, 2, 3,  5, 6, 7)])
 
 imax <- length(c5_prediction)
 c = 0;
@@ -66,6 +68,7 @@ correct <- round((c / imax) * 100,1)
 correct
 
 table(gplay$Rating)
+table(gplay$Reviews)
 table(gplay$Size)
 table(gplay$Installs)
 table(gplay$Type)
