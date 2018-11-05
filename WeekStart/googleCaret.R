32d824fa5ccbc67ef6fb156cacc5b5cd3f7339cb
Zartei <Zirdii@gmail.com>
2018-11-05 14:02:10 +0100

    NB with Caret

--- WeekStart/googleCaret.R
+++ WeekStart/googleCaret.R
@@ -4,23 +4,34 @@ source("myScripts.R")
 # Libs used.
 library(dplyr)
 library(e1071)
+library(caret)
+library(klaR)
 
 # Functions
 # Function to filter Genres to just have one value.
-myfiter <- function(x) {
-    gen <- unlist(strsplit(paste(x), "[;]"));
-    return(gsub(" ", "", gen[1], fixed = TRUE))
-}
-
-# Function to filter app names.
-filterName <- function(x) {
-    str <- iconv(paste(x), from = 'UTF-8', to = 'ASCII//TRANSLIT')
-    gen <- unlist(strsplit(str, "[ ]"));
-    return(paste(head(gen, 1), tail(gen, 1), sep = " "))
-}
 
 # Variables
 input <- "googleplaystore.csv"
 
 # Start.
 gplay <- read.csv(input)
+
+gplay <- gplay[c('Category','Rating','Reviews')]
+gplay <- gplay[-10473,]
+
+gplay$Rating <- as.factor(gplay$Rating)
+gplay$Reviews <- as.factor(gplay$Reviews)
+
+levels(gplay$Rating)[levels(gplay$Rating) == 'NaN'] <- "Missing"
+gplay <- droplevels(gplay)
+
+gplay_set <- splitTrainSet(gplay, split = 0.9)
+
+x = gplay_set$train[, -1]
+y = gplay_set$train$Category
+
+model = train(x, y, 'nb', trControl = trainControl(method = 'cv', number = 10))
+# model
+pre <- predict(model$finalModel, gplay_set$test[,-1])
+
+checkPrediction(pre$class, gplay_set$test$Category)
